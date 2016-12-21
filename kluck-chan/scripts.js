aaf39d2bfb9911d9fa8633e2947191cf49fd8bac
git-default <git-default@example.com>
2016-12-21 16:12:43 +0900

    Update 2016-12-21T07:11:26.219Z

--- kluck-chan/scripts.js
+++ kluck-chan/scripts.js
@@ -1,1 +1,1 @@
-$(document).on("click","a",function(){var c,d;return c=$(this),c.hasClass("clicked")?void 0:(d=c.find("img")[0],$("body").css({backgroundImage:"url("+d.src+")",backgroundSize:d.width+"px "+d.height+"px"}),c.addClass("clicked"),!1)});
+$(document).on("click","a",function(){var c,i;if(c=$(this),!c.hasClass("clicked"))return i=c.find("img")[0],$("body").css({backgroundImage:"url("+i.src+")",backgroundSize:i.width+"px "+i.height+"px"}),c.addClass("clicked"),!1});
