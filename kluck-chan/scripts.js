23f924b38900efab2aadb72d66d4537e93529ef7
git-default <git-default@example.com>
2017-05-27 14:57:36 +0900

    Update 2017-05-27T05:57:33.486Z

--- kluck-chan/scripts.js
+++ kluck-chan/scripts.js
@@ -1,1 +1,1 @@
-$(document).on("click","a",function(){var c,i;if(c=$(this),!c.hasClass("clicked"))return i=c.find("img")[0],$("body").css({backgroundImage:"url("+i.src+")",backgroundSize:i.width+"px "+i.height+"px"}),c.addClass("clicked"),!1});
+$(document).on("click","a",function(){var c,i;if(!(c=$(this)).hasClass("clicked"))return i=c.find("img")[0],$("body").css({backgroundImage:"url("+i.src+")",backgroundSize:i.width+"px "+i.height+"px"}),c.addClass("clicked"),!1});
